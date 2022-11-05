fbfb87eef787de556d54647c86c5dd05171d05f2
Glitch (hypnotic-synonymous-voyage) <none>
2022-11-05 17:18:35 +0000

    🍚🗾 Checkpoint ./commands/i.js:42143235/594

--- commands/i.js
+++ commands/i.js
@@ -21,7 +21,7 @@ exports.run = async (client, message, args) => {
       iconURL: client.user.avatarURL(),
     })
     .setDescription(
-      `<:tr_takvim:1023137043959795712> **Yapım Tarihi:**\n12 Eylül 2020\n<:tr_ayarlar:1023136979438796882> **Uptime:**\n${Uptime}`
+      `<:tr_takvim:1023137043959795712> **Yapım Tarihi:**\n12 Eylül 2020\n<:tr_ayarlar:1023136979438796882> **Uptime:**\n${Uptime}\n<:tr_tac:1023137042466607135> **Bot Sahibi:**\n<@677218405752766499> ve <@1005112531431469136>\n<:tr_bot:1023136986678173716> **Kütüphane:** Discord.js\n<:tr_menu:1023137020152913920> **Sunucu Sayısı:**\n${client.guilds.cache.size}\n<:tr_kesfet:1023137015350431835> **Kullanıcı Sayısı:**\n${client.users.cache.size}\n<:tr_gizli:1023137003660918844> **Geçikme Süreleri:**\n${client.ws.ping}\n<:tr_developer:1023136993019973662> **Komut Sayısı:**`
     )
     .setFooter({
       text: `Bot İstatistik`,
